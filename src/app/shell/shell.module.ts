e725a9382fe3da05f142c2ab9246b0edbdc954a6
Joel Cuthriell <joel@cuthriell.com>
2021-10-22 01:59:08 -0500

    breadcrumbs and styling

--- src/app/shell/shell.module.ts
+++ src/app/shell/shell.module.ts
@@ -8,9 +8,10 @@ import { I18nModule } from 'src/app/i18n';
 import { AuthModule } from 'src/app/auth';
 import { ShellComponent } from './shell.component';
 import { HeaderComponent } from './header/header.component';
+import { BreadcrumbModule } from 'xng-breadcrumb';
 
 @NgModule({
-  imports: [CommonModule, TranslateModule, NgbModule, AuthModule, I18nModule, RouterModule],
+  imports: [CommonModule, TranslateModule, NgbModule, AuthModule, I18nModule, RouterModule, BreadcrumbModule],
   declarations: [HeaderComponent, ShellComponent],
 })
 export class ShellModule {}
